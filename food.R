54cbf84092dcac2c1d32962028f1ddf9e75f75de
Heili Lowman <heilil@sccwrp.org>
2020-11-02 12:14:46 -0800

    Initial commit.

---
+++ food.R
@@ -0,0 +1,7 @@
+# SCCWRP Tidy Tuesday
+# Practice with using Git & GitHub
+# November 3, 2020
+
+# Who are you and what is your favorite food?
+
+# My name is Heili, and my favorite food is Thanksgiving-leftover sandwiches.
